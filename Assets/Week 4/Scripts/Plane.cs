6864bdd1aa53ad3f146545c655c472b2c9e247c9
Xiomaraze <allie.arnott@gmail.com>
2024-01-31 11:00:00 -0500

    task2 complete (pre-line debugging)

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -10,12 +10,56 @@ public class Plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastPos;
     LineRenderer lineRenderer;
+    Vector2 currentPos;
+    Rigidbody2D rigbody;
+    public float speed = 1;
+    public AnimationCurve landing;
+    float landingtimer;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+        rigbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentPos = new Vector2(transform.position.x, transform.position.y);
+        if (points.Count > 0 )
+        {
+            Vector2 direction = points[0] - currentPos;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigbody.rotation = -angle;
+        }
+        rigbody.MovePosition(rigbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            landingtimer += 0.1f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingtimer);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero * 0.75f, interpolation);
+        }
+        if (points.Count > 0)
+        {
+            if (Vector2.Distance(currentPos, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+                for (int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i+1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()
@@ -36,7 +80,7 @@ public class Plane : MonoBehaviour
         {
             points.Add(currentPosition);
             lineRenderer.positionCount++;
-            lineRenderer.SetPosition(0, currentPosition);
+            lineRenderer.SetPosition (0, currentPosition);
             lastPos = currentPosition;
         }
     }
