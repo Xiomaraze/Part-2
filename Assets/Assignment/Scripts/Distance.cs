e1e86437a04bef986d19e66f93f7d1ae6ce3c8e5
Xiomaraze <allie.arnott@gmail.com>
2024-02-20 12:33:40 -0500

    Added player object Updated distance script to record total distance traveled Attached distance script to steps taken button

--- Assets/Assignment/Scripts/Distance.cs
+++ Assets/Assignment/Scripts/Distance.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Distance : MonoBehaviour
 {
+    float distance;
+    public float distSinceLast;
+    public List<Vector2> travelPoints;
+    public GameObject pc;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,25 @@ public class Distance : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 last = travelPoints.Last();
+        float dist = Vector2.Distance(last, transform.position);
+        if (dist > distSinceLast)
+        {
+            travelPoints.Add(transform.position);
+            distance += dist;
+        }
+    }
+
+    //resets the list with the button use and sets the first point in the travel distance list to the objects current position
+    public void Restart()
+    {
+        travelPoints = new List<Vector2>();
+        Vector2 cur = transform.position;
+        travelPoints.Add(cur);
+        distance = 0;
     }
 }
